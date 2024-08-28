6d7d2150edd542fd7064bf7e6ddbd4d4900f9d43
Joaofelipe14 <jaofelpe14@gmail.com>
2024-08-28 13:00:52 -0300

    feat:

--- src/app/pages/admin/financeiro/financeiro.component.ts
+++ src/app/pages/admin/financeiro/financeiro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-financeiro',
@@ -7,8 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinanceiroComponent  implements OnInit {
 
-  constructor() { }
+  cadastrarFinancerioForm!: FormGroup;
+  options = [
+    { value: 'dia', label: 'Dia' },
+    { value: 'semanal', label: 'Semanal' },
+    { value: 'mes', label: 'Mês' }
+  ];
+  valorPeriodicidade: string = '';
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.cadastrarFinancerioForm = this.formBuilder.group({
+      periodicidade: ['', Validators.required],
+      transporte: ['', Validators.required],
+      combustivel: ['', Validators.required],
+      embarcacao: ['', Validators.required],
+      material: ['', Validators.required],
+      energia: ['', Validators.required],
+
+
+    });
+  }
+
+  onSubmit() {
+    if (this.cadastrarFinancerioForm.valid) {
+      const formData = this.cadastrarFinancerioForm.value;
+      console.log('Form Data:', formData);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  // Valor selecionado atual
+  selectOption(value: string) {
+    this.valorPeriodicidade = value;
+    this.cadastrarFinancerioForm.patchValue({ periodicidade: value });
+
+  }
 
 }
