2682c09c7fb42b39fab6d90bcc85d5a41deac494
Joaofelipe14 <jaofelpe14@gmail.com>
2024-08-30 18:24:53 -0300

    feat: :art: Atualizando url api
    Atualizando url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://43cb-2804-d4b-7753-100-ef8e-60c1-81be-4aa2.ngrok-free.app/api/'
+  apiUrl: 'https://174f-2804-d4b-7753-100-ef8e-60c1-81be-4aa2.ngrok-free.app/api/'
 
 };
