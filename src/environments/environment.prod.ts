03473ef01358714160d86cd177c9027277b9cf69
Joaofelipe14 <jaofelpe14@gmail.com>
2024-10-14 19:25:04 -0300

    feat: :art: Atualizando end point
    Atualizando end point
    
    Atualizando end point

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://aeb3-2804-d4b-7753-100-8ed9-3a22-267c-6b14.ngrok-free.app/api/'
+  apiUrl: 'https://eb06-168-121-68-156.ngrok-free.app/api/'
 
 };
