38796b924a11225f87acf301ef00523edfbe6c19
Joaofelipe14 <jaofelpe14@gmail.com>
2024-10-18 16:22:44 -0300

    feat: :art: ALetrando api
    ALetrando api
    
    ALetrando api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://522d-2804-2dbc-e076-701-683c-74d5-ee6a-8c9a.ngrok-free.app/'
+  apiUrl: 'https://522d-2804-2dbc-e076-701-683c-74d5-ee6a-8c9a.ngrok-free.app/api/'
 
 };
