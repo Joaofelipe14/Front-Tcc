9605c52604b45143b4e762136dcf68602cf552e4
Joaofelipe14 <jaofelpe14@gmail.com>
2024-11-06 14:07:15 -0300

    feat: :art: AtuLIZANDO VERSÃO
    AtuLIZANDO VERSÃO
    
    AtuLIZANDO VERSÃO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,10 @@ import { ToastrService } from 'ngx-toastr'; // Importando o Toastr
 })
 export class AppComponent implements OnInit {
 
-  constructor(private swUpdate: SwUpdate, private toastr: ToastrService) {}
+  constructor(private swUpdate: SwUpdate, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     if (this.swUpdate.isEnabled) {
-      // Verifica a versão do app toda vez que o usuário abrir o aplicativo
       this.checkVersion();
     } else {
       console.log('Service Worker não está habilitado!');
@@ -26,23 +25,20 @@ export class AppComponent implements OnInit {
     const currentAppVersion = environment.appVersion;
     console.log('Versão atual: ' + currentAppVersion);
     const storedVersion = localStorage.getItem('appVersion');
-    
+
     if (currentAppVersion !== storedVersion) {
       console.log('Nova versão detectada!');
-      
-      // Usando Toastr para informar sobre a nova versão, mas esperando o clique do usuário
+
       const toast = this.toastr.info('Uma nova versão está disponível. O app será atualizado. Clique para atualizar.', 'Nova versão', {
         timeOut: 0, // Não fechar automaticamente
-        progressBar: true, // Barra de progresso
-        closeButton: true, // Mostrar botão de fechar
-        disableTimeOut: true // Não desabilitar o clique
+        progressBar: true,
+        closeButton: true,
+        disableTimeOut: true
       });
 
-      // Usando o evento de clique no Toast para atualizar
       toast.onTap.subscribe(() => {
-        // Atualizando a versão no localStorage e recarregando a página
         localStorage.setItem('appVersion', currentAppVersion);
-        window.location.reload(); // Recarregando a página após o clique
+        window.location.reload();
       });
     } else {
       console.log('Versão já está atual.');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://290b-2804-d4b-774f-6700-60c2-3372-9266-8656.ngrok-free.app/api/',
-  appVersion: '1.0.7' 
+  appVersion: '1.0.9' 
 
 };
