398a867171946ceab19b9ed57c50dd8354e63c14
Joaofelipe14 <jaofelpe14@gmail.com>
2024-11-08 19:23:05 -0300

    revert: :art: att api
    att api
    
    att api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://df23-2804-d4b-774f-6700-2a05-1228-333f-750.ngrok-free.app/api/',
-  appVersion: '1.1.2' 
+  apiUrl: 'http://177.71.185.108/api/',
+  appVersion: '1.1.3' 
 
 };
