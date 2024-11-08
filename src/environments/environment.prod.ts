9f773194aaf6380bca6f9df8d184ac14ff37c724
Joaofelipe14 <jaofelpe14@gmail.com>
2024-11-08 14:44:17 -0300

    feat: :art: Atualizando
    Atualizando
    
    Atualizando

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://290b-2804-d4b-774f-6700-60c2-3372-9266-8656.ngrok-free.app/api/',
-  appVersion: '1.1.1' 
+  apiUrl: 'https://df23-2804-d4b-774f-6700-2a05-1228-333f-750.ngrok-free.app/api/',
+  appVersion: '1.1.2' 
 
 };
